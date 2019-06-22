5900e5cac5e6f8c3c58ad28c2608195b1e931ae3
shishirkulkarni <shishirvk@gmail.com>
2019-06-22 19:28:29 -0400

    initial commit

---
+++ src/main/java/com/org/aexp/springbootPostgreSqListener/SpringBootPostgresqlListenerApplication.java
@@ -0,0 +1,17 @@
+package com.org.aexp.springbootPostgreSqListener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Spring-Boot app which listens to the PostgreSQLDB 
+ * @author shishirkulkarni
+ *
+ */
+@SpringBootApplication
+public class SpringBootPostgresqlListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPostgresqlListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/aexp/springbootPostgreSqListener/SpringBootPostgresqlListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.aexp.springbootPostgreSqListener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgresqlListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
